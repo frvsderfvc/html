5eaf8c3783701dbffaf89b7340fc6a7b747c6a89
frvsderfvc <hue22.2@outlook.com>
2026-08-26 11:46:36 +0800

    Add files via upload

---
+++ 4/BA右键菜单版.zip - 蓝奏云_files/pc3.js
@@ -0,0 +1,4 @@
+document.getElementById('jingshi').innerHTML='<div style="color: #8a6d3b;background-color: #fcf8e3;padding: 5px;border: 1px solid #faebcc;border-radius: 3px;font-size: 12px;clear: both;width: 400px;margin: auto;text-align: center;line-height: initial;margin-bottom: 10px;">反诈提示：刷单兼职，冒充公检法，共享手机屏幕，裸聊敲诈，赌博等都是诈骗，请立即举报</div>';
+document.getElementById('top').innerHTML='<div class="d1"><div class="d11 bgimg"><a href="/" class="d7">&nbsp;</a></div><div class="d12" id="d12"><a href="/account.php?action=login">登录</a></div></div>';
+var urls ='/q/jb/?f='+fid+'&report=1';
+document.getElementById('foot_info').innerHTML='<div class="foot_copy">版权声明：不得利用云存储发布，存储，淫秽，诈骗等违法信息。<a href="'+urls+'" target="_blank">侵权/违法投诉</a><br>或者投诉邮箱：ta@lanzou.com</div>&copy; 2026 Lanzou A';
